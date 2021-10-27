86f3831cff1f49bf1d0232f03faec20615c072d4
fleutius <flemming88@gmail.com>
2021-10-27 18:51:27 +0200

    v2

--- main.py
+++ main.py
@@ -1,101 +1,188 @@
-'''
-Projekt 2 - Uge 43
-Bar Røv Python program
-Flemming, Elisabeth, Anders, Daniel Egelund
-'''
-
-# IMPORTS
-from bar_resources import Dice
-from bar_resources import retry
-from bar_resources import Player
-import sys
 import random
-
-# Spilles med 5 terninger(6 sider) og mindst 2 spillere
-# REGLER:
-# Så mange point som muligt og først komme op på fas
-#
-#
-# 5 alm terninger
+import sys
 
 
+class Die:
+    dice = []
 
-def dice_roll():
-    dice_list = [d1, d2, d3, d4, d5]
+    def __init__(self, name):
+        self.name = name
+        self.index = 0
+        self.type = 6
+        self.locked = False
+        self.value = 0
+        self.dice.append(self)
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    def __iter__(self):
+        return self.name
+
+    def __next__(self):
+        if self.index < len(self.dice):
+            result = (self.name[self.index])
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
+
+    def toggle(self):
+        self.locked = False if self.locked else True
+
+    def roll(self):
+        if self.locked is False:
+            self.value = random.randint(1, self.type)
+
+    def get_value(self):
+        return self.value
+
+    @classmethod
+    def add_dice(cls, i):
+        for t in range(i):
+            die = Die(f'Terning {t + 1}')
+
+
+class Player:
+    players = []
+
+    def __init__(self, name):
+        self.name = name
+        self.index = len(Player.players)
+        self.turns = 0
+        self.score = 0
+        self.active = False
+        self.players.append(self)
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.players):
+            result = (self.name[self.index])
+            self.index += 1
+            return result
+        elif self.index == len(self.players):
+            self.index = 0
+        else:
+            raise StopIteration
+
+    @classmethod
+    def add_player(cls, i):
+        for t in range(i):
+            player = Player(f'Player {t + 1}')
+
+    def get_score(self):
+        return self.score
+
+    def add_score(self, scored_points):
+        self.score += scored_points
+
+    @classmethod
+    def list_players(cls):
+        for player in cls.players:
+            print(player)
+
+
+score_limit = 0
+score_limit_input = input("Hvad skal der spilles til a/b/c = (Aa-50 point) (b - 100 point) (c - 150 point)")
+score_limit_lower = score_limit_input.lower()
+try:
+    if score_limit_lower == "a":
+        score_limit = 50
+        print(f'score limit sat til {score_limit}')
+    if score_limit_lower == "b":
+        score_limit = 100
+        print(f'score limit sat til {score_limit}')
+    if score_limit_lower == "c":
+        score_limit = 150
+        print(f'score limit sat til {score_limit}')
+except:
+    print("Venligst intast enten a, b eller c ")
+
+
+def throw():
     avail_dice = 0
-    player_score = 0
     times_rolled = 0
-    for d in dice_list:
-        if d.locked is False:
+    score = 0
+    for die in Die.dice:
+        if die.locked is False:
             avail_dice += 1
-    print(f'{avail_dice} avail_dice')
+    print(f'Der er {avail_dice} ulåste terninger ')
     while avail_dice >= 1:
-        for i in range(avail_dice):
-            d.roll()
-            this_roll = []
-            if d.get_value() == 2:
+        for dice in range(avail_dice):
+            die.roll()
+            if die.get_value() == 2:
                 avail_dice -= 1
                 times_rolled += 1
-                print(f'der blev rullet {d.get_value()}')
-                print("Terningen ramte 2, og gemmes")
-                print()
-            elif d.get_value() == 5:
+                print(f'der blev rullet {die.get_value()} - Terningen gemmes')
+            elif die.get_value() == 5:
                 avail_dice -= 1
                 times_rolled += 1
-                print(f'der blev rullet {d.get_value()}')
-                print("Terningen ramte 5, og gemmes")
-                print()
+                print(f'der blev rullet {die.get_value()} - Terninging gemmes')
+
             else:
-                player_score += d.value
+                score += die.get_value()
                 times_rolled += 1
-                print(f'der blev rullet {d.get_value()} din score er nu {player_score}')
-                print()
 
-            print(f'der er {avail_dice} terninger tilbage')
-
-    print(f'Der blev rullet i alt {times_rolled} gange, og spiller ')
-
-def player_turn():
-    player_count = int(input("Hvor mange spillere er i: "))
-    player_list = []
-    active_player = []
-    for x in range(player_count):
-        player_name = f'player{x+1}'
-        player = Player(player_name)
-        player_list.append((f'player{x+1}'))
-    print(str(player_list))
-
-def main_test():
-    points_needed = int(input("Hvor mange point skal spillet gå til (50, 100, eller vælg selv): "))
-    player_count = int(input("Hvor mange spillere er i: "))
-    player_list = []
-    d1 = Dice(6)
-    d2 = Dice(6)
-    d3 = Dice(6)
-    d4 = Dice(6)
-    d5 = Dice(6)
-    for i in range(player_count):
-        player_list.append("player {}".format(i + 1))
-
-    dice_roll()
+                print(f'der blev rullet {die.get_value()} - din score er nu {score}')
+    print()
+    print(f'Alle terninger er nu låst. Din samlede score i runden var {score} ')
+    print(f'Du rullede i alt {times_rolled} gange')
+    return score
 
 
+def play_round():
+    for player in Player.players:
+        print()
+        print(f'Det er spiller {player} tur')
+        turn_score = throw()
+        player.add_score(turn_score)
+        print(player.name, "score -", player.score)
+        next(player)
+
+    for player in Player.players:
+        if player.score >= score_limit:
+            winner = max(Player.players, key=lambda player: player.score)
+            print()
+            print(f'Spillet er slut!. {winner} har vundet')
+            for player in Player.players:
+                print(f'{player.name} fik - {player.score} point!')
+            #            retry()
+            sys.exit()
+        else:
+            print()
+            print("Resultater for runden: ")
+            for player in Player.players:
+                print(f'{player.name} er på - {player.score} point!')
+            print("Ny runde starter: ")
+            print()
+            play_round()
 
 
+def retry():
+    print()
+    tryagain = input("Vil du prøve igen? Y/N: ")
+    retry = tryagain.lower()
+    if retry == "y":
+        main()
+    else:
+        print()
+        print("Tak fordi du spillede Bar Røv. Spillet lukkes")
+        sys.exit()
 
 
+def main():
+    Die.add_dice(5)  # Adding dice to game
+    p = int(input("Hvor mange spillere er i? "))
+    try:
+        Player.add_player(p)
+    except ValueError:
+        print("Fejl i antal spillere, venligst intast et helt tal! ")
+    play_round()  # Plays first round with all players
 
 
-player_turn()
-
-# Point fastsættelse (50, 100, ...)
-# Fastsætter antal spillere
-# Spiller efter tur, første slag = ALLE terninger
-
-# Slag med  terninger med værdi på enten 2 eller 5, giver IKKE point, og terninger med 2 eller 5 låses
-# Der slåes med  de resterende terninger
-# Et slag uden 2 eller 5 værdier, giver point, summen af øjnene, er point
-# Der slåes indtil alle terninger er væk (låst)
-# Når alle terninger har værdi på enten 2 eller 5, er det BAR RØV, og tur går videre
-# Sum af point noteres for hver afsluttet runde
-# Når der i en runde er mindst 1 spiller som har opnået aftalt point, spilles runden færdig, og point sum afgør spil
+main()
